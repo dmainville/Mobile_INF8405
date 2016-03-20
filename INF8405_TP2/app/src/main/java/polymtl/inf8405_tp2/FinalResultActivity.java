2de873b9c9a434bc0bee6a52fc0738c57fc7f34c
Rafi Hagopian <Rafi Hagopian>
2016-03-20 13:11:35 -0400

    Ajout de toutes les activités (vides)

---
+++ INF8405_TP2/app/src/main/java/polymtl/inf8405_tp2/FinalResultActivity.java
@@ -0,0 +1,13 @@
+package polymtl.inf8405_tp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FinalResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_result);
+    }
+}
---
+++ INF8405_TP2/app/src/main/java/polymtl/inf8405_tp2/VoteMapActivity.java
@@ -0,0 +1,13 @@
+package polymtl.inf8405_tp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VoteMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vote_map);
+    }
+}
